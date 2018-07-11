7de45a541af8146f2587e58a5a714678be397c60
Wako210 <w.seubring@gmail.com>
2018-07-11 16:00:47 +0200

    added pre and post to DFS

--- Graph.py
+++ Graph.py
@@ -1,15 +1,13 @@
 from Vertex import Vertex
 
 
-
-
-
 class Graph:
     def __init__(self, is_directed=True):
         self.name = None
         self.vertexes = dict()
         self.is_directed = is_directed
         self.n_edges = 0
+        self.count = 0
 
     def add_vertex(self, name):
         self.vertexes[str(name)] = Vertex(name)
@@ -24,18 +22,10 @@ class Graph:
         if not self.is_directed:
             vertex_b.add_neighbour(vertex_a)
 
-    def __str__(self):
-        text = ""
-        for item in self.vertexes:
-            text += '{} -> ['.format(item)
-
-            for vertex in self.vertexes[item].connections_to:
-                text += "{}, ".format(str(vertex.name))
-                pass
-            text += ']\n'
-        return text
-
     def dfs(self, vertex_name_a, vertex_name_b):
+        self.count = 0
+        self.reset_visited_vertexes()
+
         begin_vertex = self.vertexes[str(vertex_name_a)]
         target = self.vertexes[str(vertex_name_b)]
 
@@ -48,7 +38,10 @@ class Graph:
 
     def __dfs_recursion(self, vertex, target, path):
         path += "{}->".format(str(vertex.name))
+
         vertex.visited = True
+        vertex.pre = self.count
+        self.count += 1
 
         if vertex == target:
             return path
@@ -60,6 +53,9 @@ class Graph:
         for possible_vertex_other_cluster in self.vertexes.values():
             if not possible_vertex_other_cluster.visited:
                 return self.__dfs_recursion(possible_vertex_other_cluster, target, "[")
+
+        vertex.post = self.count
+        self.count += 1
 
     def num_of_clusters(self):
         if len(self.vertexes) == 0:
@@ -80,3 +76,20 @@ class Graph:
                 return 1 + self.__num_of_clusters_recursion(possible_vertex_other_cluster)
 
         return 0
+
+    def reset_visited_vertexes(self):
+        for vertex in self.vertexes:
+            vertex.visited = False
+
+    def __str__(self):
+        text = ""
+        for item in self.vertexes:
+            text += '{} -> ['.format(item)
+
+            for vertex in self.vertexes[item].connections_to:
+                text += "{}, ".format(str(vertex.name))
+                pass
+            text += ']\n'
+        return text
+
+
--- Vertex.py
+++ Vertex.py
@@ -1,9 +1,13 @@
+import math
+
 class Vertex:
     def __init__(self, name):
         self.name = name
         self.connections_to = []
         self.visited = False
         self.prev = None
+        self.pre = None
+        self.post = None
 
     def add_neighbour(self, neighbour):
         self.connections_to.append(neighbour)
